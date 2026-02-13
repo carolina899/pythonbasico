0ff38df64e8b77e995f3fad53cc9a0837edc1dc5
carolina899 <carolina89209@gmail.com>
2026-02-12 18:54:23 -0800

    tuplas y listas

--- sesion_02.py
+++ sesion_02.py
@@ -7,3 +7,14 @@ mi_lista = [1, 3.1416, "carolina", mi_tupla]
 print("el primer elemento  de mi lista: ", mi_lista[0])
 print("el primer elemento  de mi lista: ", mi_lista[3])
 print("el primer elemento  de mi lista: ", mi_lista[2])
+
+#diccionarios
+mi_diccionario = {
+    "mi_lista": mi_lista,
+    "nombre" : "carolina",
+    "pi": 3.1416,
+    "tel":"664-585-4256"
+}
+print("llave para accesar a mi diccionario mi lista", mi_diccionario[mi_lista])
+print("llave para accesar a mi diccionario pi", mi_diccionario["pi"])
+print("llave para accesar a mi diccionario tel", mi_diccionario["tel"])
