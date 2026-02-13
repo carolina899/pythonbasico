99d49c0c8110a6128f52788ddd1d98b0f8a2a7a0
carolina899 <carolina89209@gmail.com>
2026-02-12 18:43:48 -0800

    tuplas y listas

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#tuplas
+mi_tupla = (2,4)
+print("mi dupla: ", mi_tupla)
+
+#lista
+mi_lista = [1, 3.1416, "carolina", mi_tupla]
+print("el primer elemento  de mi lista: ", mi_lista[0])
+print("el primer elemento  de mi lista: ", mi_lista[3])
+print("el primer elemento  de mi lista: ", mi_lista[2])
