31d444fbf1641b1fe962bbcfcedef6ed56b68381
carolina899 <carolina89209@gmail.com>
2026-02-23 21:35:20 -0800

    Add files via upload

---
+++ funcion_calculadora.py
@@ -0,0 +1,22 @@
+print("========Mi Super Calculadora==========")
+num_1 = float(input("Escriba el valor del primer numero: "))
+num_2 = float(input("Escriba el valor del segundo numero: "))
+operacion = input("¿Cual operacion deseas hacer? +, -, *, / -> ")
+
+def calculadora(num_1, num_2, operacion):
+    if operacion == "+":
+        return num_1 + num_2
+    elif operacion == "-":
+        return num_1 - num_2
+    elif operacion == "*":
+        return num_1 * num_2
+    elif operacion == "/":
+        if num_2 != 0:
+            return num_1 / num_2
+        else:
+            return "error division con 0"    
+    else:
+        return "error en la operacion"
+    
+resultado = calculadora(num_1, num_2, operacion) 
+print("El resultado es:", resultado)
