4f4fc8eadcebba213260a1d4ccddd1b6e3abb89a
carolina899 <carolina89209@gmail.com>
2026-02-26 19:20:52 -0800

    carpeta de objetos

--- oop/ogro.py
+++ oop/ogro.py
@@ -1,8 +1,8 @@
 from enemigo import *
 
-class ogro(Enemigo):
+class Ogro(Enemigo):
     def __init__(self, puntos_energia=20, ataque=3):
-        super().__init__(tipo_enemigo=ogro, puntos_energia=puntos_energia, ataque=ataque)
+        super().__init__(tipo_enemigo="Ogro", puntos_energia=puntos_energia, ataque=ataque)
 
-        def habla(self):
-            print("ogro aplasta todo!!!")
+    def habla(self):
+        print("ogro aplasta todo!!!")
--- oop/zombie.py
+++ oop/zombie.py
@@ -1,11 +1,11 @@
 from enemigo import *
 
-class zombie(Enemigo):
-    def _init_(self,puntos_energia=10,ataque=1):
-        super()._init_(tipo_enemigo='zombie',puntos_energia=puntos_energia,ataque=ataque)
+class Zombie(Enemigo):
+    def __init__(self, puntos_energia=10, ataque=1):
+        super().__init__(tipo_enemigo="Zombie", puntos_energia=puntos_energia, ataque=ataque)
 
-        def habla(self):
-            print("*Hummmm........*")
+    def habla(self):
+        print("*Hummmm........*")
 
-            def propagar_enfermedad(self):
-                print("El zombie eata tratando de propagar la enfermedad!!")
+    def propagar_enfermedad(self):
+        print("El zombie eata tratando de propagar la enfermedad!!")
