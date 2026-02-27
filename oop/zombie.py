323f15ee16ebafd939712e06f929809d4c7212be
carolina899 <carolina89209@gmail.com>
2026-02-27 15:48:45 -0800

    actualizacion de pelea

--- oop/zombie.py
+++ oop/zombie.py
@@ -1,4 +1,5 @@
 from enemigo import *
+import random
 
 class Zombie(Enemigo):
     def __init__(self, puntos_energia=10, ataque=1):
@@ -9,3 +10,10 @@ class Zombie(Enemigo):
 
     def propagar_enfermedad(self):
         print("El zombie eata tratando de propagar la enfermedad!!")
+
+    def ataque_especial(self):
+       print("zombie ataque especial")
+       funciona_ataque_especial = random.random() < 0.5
+       if funciona_ataque_especial:
+        self.ataque += 2
+        print('zombie ha regenerado su energia con 2HP!!!!')
